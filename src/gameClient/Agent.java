13f2e8573e24e6d7ba5fe9c76674da155e4af3e2
eloblo <eloblo250@gmail.com>
2020-12-13 15:54:36 +0200

    improved code

--- src/gameClient/Agent.java
+++ src/gameClient/Agent.java
@@ -2,6 +2,8 @@ package gameClient;
 
 import api.*;
 
+import java.util.HashSet;
+
 /** the class is Runnable object for CL_Agent class.
  *  calls it's specific agent to move according to it's
  *  position and sleep until the agent is calculated to
@@ -10,6 +12,9 @@ import api.*;
 public class Agent implements Runnable {
 
     private CL_Agent _agent;           //game agent
+    private CL_Pokemon _prevPok;       //previous targeted pokemon
+    private edge_data _prevEdge;       //the previous edge the agent visited
+    private HashSet<String> whiteList; //list of pokemon that are allowed to be targeted
     private static game_service _game; //game service
     private static Mover _mover;       //the mover object
 
@@ -17,13 +22,14 @@ public class Agent implements Runnable {
         _agent = agent;
         _game = game;
         _mover = mover;
+        whiteList = new HashSet<>();
     }
 
     @Override
     public void run() {
         long dt = 0;
         while(_game.isRunning()){
-            dt = _mover.init(_agent);   //tell the agent to move and receives
+            dt = _mover.init(this, _agent);   //tell the agent to move and receives
             try {                       //sleeping time to reduce unnecessary movement calls
                 Thread.sleep(dt);
             }
@@ -31,5 +37,28 @@ public class Agent implements Runnable {
                 e.printStackTrace();
             }
         }
+    }
+    public CL_Pokemon getPrevPok() {
+        return _prevPok;
+    }
+
+    public void setPrevPok(CL_Pokemon _prevPok) {
+        this._prevPok = _prevPok;
+    }
+
+    public edge_data getPrevEdge() {
+        return _prevEdge;
+    }
+
+    public void setPrevEdge(edge_data _prevEdge) {
+        this._prevEdge = _prevEdge;
+    }
+
+    public HashSet<String> getWhiteList() {
+        return whiteList;
+    }
+
+    public void setAgent(CL_Agent agent) {
+        _agent = agent;
     }
 }
--- src/gameClient/Mover.java
+++ src/gameClient/Mover.java
@@ -2,7 +2,6 @@ package gameClient;
 
 import api.*;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,33 +14,28 @@ public class Mover {
 
     private static Arena _ar;      //game arena
     private CL_Agent _agent;       //the agent being worked on
+    private Agent _agT;            //the current agent's thread
     private CL_Pokemon _pokemon;   //the targeted pokemon
     private CL_Pokemon _prevPok;   //previous targeted pokemon
     private edge_data _prevEdge;   //the previous edge the agent visited
     private static HashSet<String> _blackList;      //black list of pokemon the agents cant target
-    private static HashMap<Integer,HashSet<String>> _whiteList;  //white list of what pokemon only this agent can target
-    private static HashMap<Integer,edge_data> _prevEdges;        //map of prevEdge to each agent
-    private static HashMap<Integer,CL_Pokemon> _prevPkList;      //map of prevPok to each agent
-    private static directed_weighted_graph _graph;         //game graph
-    private static DWGraphs_Algo _graphAlgo;      //graph algorithms to calculate paths
-    private static game_service _game;        //game service
+    private static HashSet<String> _whiteList;      //list of pokemon that are allowed to be targeted
+    private static directed_weighted_graph _graph;  //game graph
+    private static DWGraphs_Algo _graphAlgo;        //graph algorithms to calculate paths
+    private static game_service _game; //game service
     private static int _reset = 0;     //reset counter to reset blackList in case of congestions
+    private static int _AC;  //the count of agents
 
     /** constructor of mover. sets all the data from the agents */
     public Mover(Arena ar, directed_weighted_graph graph, game_service game, int numOfAgents){
         _ar = ar;
         _graph = graph;
         _game = game;
+        _AC = numOfAgents;
         _blackList = new HashSet<>();
-        _whiteList = new HashMap<>();
         _graphAlgo = new DWGraphs_Algo(new DWGraph_DS());
-        _prevEdges = new HashMap<>();
-        _prevPkList = new HashMap<>();
         DWGraph_DS temp = _graphAlgo.caster(_graph);
         _graphAlgo.init(temp);
-        for(int i = 0; i < numOfAgents; i++){
-            _whiteList.put(i,new HashSet<String>());
-        }
     }
 
     /** the central method of mover.
@@ -50,12 +44,14 @@ public class Mover {
      * @param agent the mover is currently working on.
      * @return the sleeping time of the agent.
      */
-    public synchronized int init(CL_Agent agent){
+    public synchronized int init(Agent thread, CL_Agent agent){
         _agent = agent;                             //set the agent and pulls the relevant data
-        _prevEdge = _prevEdges.get(_agent.getID());
-        _prevPok = _prevPkList.get(_agent.getID());
+        _agT = thread;
+        _prevEdge = thread.getPrevEdge();
+        _prevPok = thread.getPrevPok();
+        _whiteList = thread.getWhiteList();
         int nextNode = moveAgents();               //move the agent
-        if(_reset == 100* _whiteList.size()){      //if reached congestion cap reset
+        if(_reset == 100* _AC){      //if reached congestion cap reset
             resetLists();
         }
         double slpTime = 100;    //default sleep time
@@ -99,8 +95,8 @@ public class Mover {
             double ratio = dist2Pok/edgeDist;   //calculate the ratio between the distance to the target compare to the edge
             weight = pokEdge.getWeight();
             weight *= ratio;                    //adjust the distance to the weight of the
-            _prevEdges.put(_agent.getID(), currEdge);
-            _prevPkList.put(_agent.getID(), _pokemon);
+            _agT.setPrevEdge(currEdge);
+            _agT.setPrevPok(_pokemon);
         }
         //after collecting all the pokemon on the edge and the agent is still on the edge calculate the time to reach the next node
         else if(_prevEdge != null && _prevEdge.getDest() == _prevPok.get_edge().getDest() && _prevEdge.getSrc() == _prevPok.get_edge().getSrc()){
@@ -114,7 +110,7 @@ public class Mover {
             double dist2Dest = destNodePos.distance(_agent.getLocation());//calculate the distance to the next node
             double ratio = dist2Dest/edgeDist;                            //ratio of the distance to the edge
             weight *= ratio;                                              //adjust the weight to the to the distance
-            _prevEdges.put(_agent.getID(),_pokemon.get_edge());           //reset the prevEdge
+            _agT.setPrevEdge(_pokemon.get_edge());           //reset the prevEdge
         }
         weight *= 1000;                 //convert the weight and speed to milliseconds
         double slpTime = weight/speed;
@@ -134,6 +130,7 @@ public class Mover {
         List<CL_Pokemon> ffs = Arena.json2Pokemons(fs);
         _ar.setPokemons(ffs);
         _agent = log.get(_agent.getID());   //update the agent
+        _agT.setAgent(_agent);
         int id = _agent.getID();
         int src = _agent.getSrcNode();
         double v = _agent.getValue();
@@ -172,7 +169,7 @@ public class Mover {
         double minDist = Integer.MAX_VALUE;  //max value for comparison
         for (CL_Pokemon pok : pokes) {       //search for the closest pokemon
             String pos = pok.getLocation().toString();  //used to identify the pokemon as they don't have id
-            if(!_blackList.contains(pos) || _whiteList.get(_agent.getID()).contains(pos)){  //check if the pokemon isn't targeted
+            if(!_blackList.contains(pos) || _whiteList.contains(pos)){  //check if the pokemon isn't targeted
                 edge_data edge = pok.get_edge();
                 int edgeSrc = edge.getSrc();
                 int edgeDest = edge.getDest();
@@ -194,7 +191,7 @@ public class Mover {
         }
         if(_pokemon != null){  //if there is a relevant target, reserve it
             _blackList.add(_pokemon.getLocation().toString());
-            _whiteList.get(_agent.getID()).add(_pokemon.getLocation().toString());
+            _whiteList.add(_pokemon.getLocation().toString());
             _reset++;  //increased congestion
         }
         return ans;
@@ -210,7 +207,7 @@ public class Mover {
         CL_Pokemon pokemon = null;            //a fictive replacement for _pokemon
         for (CL_Pokemon pok : pokes) {        //search for closest pokemon
             String pos = pok.getLocation().toString();  //used to identify the pokemon as they don't have id
-            if(!_blackList.contains(pos) || _whiteList.get(_agent.getID()).contains(pos)){ //check if the pokemon isn't targeted
+            if(!_blackList.contains(pos) || _whiteList.contains(pos)){ //check if the pokemon isn't targeted
                 edge_data edge = pok.get_edge();
                 int edgeSrc = edge.getSrc();
                 double value = edge.getWeight();
@@ -227,7 +224,7 @@ public class Mover {
         }
         if(pokemon != null){    //if there is a relevant target reserve it
             _blackList.add(pokemon.getLocation().toString());
-            _whiteList.get(_agent.getID()).add(pokemon.getLocation().toString());
+            _whiteList.add(pokemon.getLocation().toString());
             _reset++;   //increased congestion
         }
     }
@@ -235,9 +232,7 @@ public class Mover {
     //resets all the list to reset pokemon congestion.
     private void resetLists(){
         _blackList.clear();
-        for(int i = 0; i < _whiteList.size(); i++){
-            _whiteList.get(i).clear();
-        }
+        _whiteList.clear();
         _reset = 0;
     }
 }
